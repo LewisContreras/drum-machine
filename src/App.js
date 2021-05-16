1fef74c47f2c17710d06adeef076f57c5736bfe4
LewisContreras <legiconba4@gmail.com>
2021-05-15 21:46:07 -0500

    termino los requisitos, falta diseño

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import dataSounds from "./dataSounds"
 
 function App() {
+
+  const handleClick = (e) =>{
+    let display = document.getElementById("display");
+    e.target.firstElementChild.play();
+    display.textContent = e.target.id
+  }
+
+  const handleKeyUp = (e) =>{
+    let button = document.getElementById(`${e.keyCode}`)
+    if(button){ 
+      button.firstElementChild.play()
+      let display = document.getElementById("display");
+      display.textContent = button.id
+    }  
+  }
+  
+  document.addEventListener("keyup",handleKeyUp)
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="wrapper">
+      <div id="drum-machine" >
+      <div id="display" >holi</div>
+      <div id="drums-container" >
+
+      </div>
+      {
+        dataSounds.map(el =><div id={el.keyCode} name={el.id} key={el.id} onClick={handleClick} className="drum-pad" >{el.keyTrigger}
+        <audio id={el.keyTrigger} className="clip" src={el.url}></audio>
+      </div>)
+      }
+      </div>  
     </div>
   );
 }
-
 export default App;
